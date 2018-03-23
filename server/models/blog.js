c99e7e6c9b5aefcda887ba30267649f52a86246c
David Hong <dxcdivad@gmail.com>
2018-03-23 09:12:22 -0700

    v3.1

--- server/models/blog.js
+++ server/models/blog.js
@@ -10,4 +10,4 @@ const BlogSchema = new Schema({
     author: { type: Schema.Types.ObjectId, ref: "User" }
 });
 
-module.exports = mongoose.model("Blog", BlogSchema);
+module.exports = mongoose.model("Blog", BlogSchema); 
